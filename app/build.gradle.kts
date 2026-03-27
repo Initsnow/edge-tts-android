3046a9f3170f1adf9e3e4d2dbff5f7675332ab4b
Initsnow <79002121+Initsnow@users.noreply.github.com>
2026-03-28 00:44:39 +0800

    Bump app to v0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
         applicationId = "top.initsnow.edge_tts_android"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
